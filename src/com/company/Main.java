39f2b5779fdeb0929c6beddd0c884a76408c80dc
USER <dev,elop.02@mail.ru>
2020-09-16 19:37:30 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    /*"""Scanner - получение данных от пользователя"""
+	    * """Математические операции"""
+
+        Scanner str = new Scanner(System.in);
+        float num1, num2, result;
+        System.out.print("Введите число a = ");
+        num1 = str.nextFloat();
+        System.out.print("Введите число b = ");
+        num2 = str.nextFloat();
+        result = num1 + num2;
+        System.out.print("Ответ: " + result);
+
+	     */
+
+
+
+                /*"""Условные операторы"""*/
+
+        
+    }
+}
